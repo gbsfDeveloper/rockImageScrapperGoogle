91617650e93208e44d482c65248e075759df1519
gelacio <gbsf.software.dev@gmail.com>
2020-10-01 16:59:52 -0500

    images can be scrap , images can be show inside of he web page

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const puppeteer = require('puppeteer');
+const express = require('express');
+const express_layouts = require('express-ejs-layouts'); 
+let ejs = require('ejs');
+const server = express();
+
+// settings
+server.set('port','4000');
 
 async function getImage(searchName,howMany=1){
     const urlImages = `https://www.google.com/search?q=${searchName}+roca&sxsrf=ALeKk00JBA_VepXKAmlh-lVJ2v_nxqAeyA:1600803164118&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiI08m_wP3rAhUKT6wKHVwKBpkQ_AUoAXoECBUQAw&biw=1516&bih=425`;
@@ -11,6 +18,7 @@ async function getImage(searchName,howMany=1){
     var imageb64 = [];
     if(howMany !== 1){
         for(let count=1 ; count <= howMany; count++){
+            await page.waitForSelector('img[jsname=Q4LuWd]');
             imageb64[count-1] = await page.evaluate(el => el.src, searchValue[count-1]);
         }
     }
@@ -19,8 +27,25 @@ async function getImage(searchName,howMany=1){
     }
     console.log("Imagen(es) encontrada(s)");
     await browser.close();
-    console.log(imageb64);
-    console.log(imageb64.length);
+    return imageb64;
 }
 
-getImage("arcilla",2);
+// view settings
+server.use(express.static('public'));
+server.use('/css',express.static(__dirname + 'public/css'));
+server.use('/js',express.static(__dirname + 'public/js'));
+server.use('/img',express.static(__dirname + 'public/img'));
+server.use(express_layouts);
+server.set('view engine','ejs');
+
+// Routes
+server.get("/",(req,res) => {
+    getImage("test",20).then(function(imageb64){
+        res.render('index',{array:imageb64});
+    });
+});
+
+const port = server.get('port');
+server.listen(port,() => {
+    console.log(`Listen on port ${port}`);
+});
